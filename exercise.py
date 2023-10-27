ff1a10769c2a4c233c57d6e0113e95076fd111ab
zollastar <146174920+zollastar@users.noreply.github.com>
2023-10-27 16:24:04 +0700

    Create exercise.py

---
+++ exercise.py
@@ -0,0 +1,13 @@
+def average(num1: float, num2: float) -> float:
+    """Return the average of num1 and num2.
+    >>> average(10,20)
+    15.0
+    >>> average(2.5, 3.0)
+    2.75
+    """
+    return (num1 + num2)/ 2
+
+# it will report if tests are failed when executed:
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
