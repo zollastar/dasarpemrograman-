69ab296419def8e4ec97a55e509e00dda26d005f
zollastar <146174920+zollastar@users.noreply.github.com>
2023-10-27 16:37:02 +0700

    Create math_operations.py

---
+++ math_operations.py
@@ -0,0 +1,21 @@
+# streamlit_calculator.py
+
+import streamlit as st
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        raise ZeroDivisionError("Division by zero is not allowed.")
+    return x / y
